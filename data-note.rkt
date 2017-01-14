05c641169a18c37d28b64064dfc67cdf9ed88ffe
Jonas Winje <jonas.winje@gmail.com>
2017-01-14 14:50:08 +0100

    stuff, writing

--- data-note.rkt
+++ data-note.rkt
@@ -4,11 +4,11 @@
 
 (define data-note
   (nt 'something-data
-      "Something about data"
+      "Something about data, values"
       "bla bla data something"
       @p{
  @lnk["https://mitpress.mit.edu/books/little-mler"]{The Little MLer} is a pretty fun book.
- There’s a lot of Standard ML-code in it and Standard ML code has a lot of @tt{fun} in it.
+ There’s a lot of Standard ML code in it and Standard ML code has a lot of @tt{fun} in it.
 }
       @p{
  Anyway there’s this theme in the book. We define a bunch of datatypes, and we define functions on them.
@@ -16,7 +16,7 @@
 }
 
       @p{
- Like, we introduce a recursive type, like one for natural numbers. Something along the lines of,
+ Like, we can introduce a recursive type, like one for natural numbers:
 }
 
       @block{
@@ -25,7 +25,7 @@
    | Successor of natural
 }
       @p{
- and then when we wanna do stuff with them, we kind of necessarily get recursive functions,
+ And then when we wanna do stuff with the numbers, we kind of necessarily get recursive functions, like:
  }
       @block{
  fun add (a, b) =
@@ -34,10 +34,22 @@
        | Successor x => Successor (add (x, b))
 }
       @p{
- and so something like @tt{add (Successor (Successor Zero), Successor Zero)} evaluates to something like @tt{Successor (Successor (Successor Zero))}. Woop woop.
+ (And so something like @tt{add (Successor (Successor Zero), Successor Zero)} evaluates to something like @tt{Successor (Successor (Successor Zero))}. Woop woop.)
+}
+      @p{
+ Stuff like that. A type with more constructors typically leads to pattern matching with more patterns. Mutually recursive types lead to mutually recursive functions. …
 }
       brk
       @p{
- Bla bla bla
+ (I guess this is at least pretty close to the idea of types being defined by their introduction and elimination rules, and the computational building blocks of a language being those rules.
+ Every bit of behaviour being constructing and destructing of values.)
+}
+      brk
+      @p{
+ So, I think that suggests a way of doing things where you make sure the values you’re dealing with support the behaviour you’re aiming for.
+ And then you can kind of tell that things are might be on the right track if the structure of the values show up in the structure of the functions.
+}
+      @p{
+ (Perhaps as an alternative to a way of doing things where you have some strings and booleans more like, lying around, and, like, make do.)
 }
       ))
