c512160801358b849cede50ec9668b1fc47e9ec5
Jonas Winje <jonas.winje@gmail.com>
2018-05-06 14:54:18 +0200

    mlep

--- flat18.rkt
+++ flat18.rkt
@@ -16,6 +16,8 @@
  Elm code is @lnk["https://github.com/Glorp/elm-beep-boop"]{here}.
  Some JavaScript and HTML that uses the Elm code is
  @lnk["https://github.com/llama-the-ultimate/llama-the-ultimate.github.io/tree/master/slides/flatmap2018/beep-boop"]{here}.
+ (The the more graphical parts are mostly code stolen from Einar’s
+ @lnk["https://github.com/einarwh/escher-workshop"]{Escher-in-Elm Workshop}.)
 }
    @p{
  Various beep boop evaluators. Maybe they work in some of the browsers. The later ones are more fun...
